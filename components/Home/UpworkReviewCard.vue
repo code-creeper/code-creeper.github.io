c9597cd5f9f3c16e339c06002217ed194c09035a
Abdul Haseeb Khan <abdulhaseebkhan407@gmail.com>
2026-03-25 05:23:46 +0500

    animate review card

--- components/Home/UpworkReviewCard.vue
+++ components/Home/UpworkReviewCard.vue
@@ -20,20 +20,58 @@ const comment = computed(() => {
 const hasMore = props.review.feedbacks?.length > 1
 const moreCount = props.review.feedbacks?.length - 1
 
-const seeMoreToggle = (event : MouseEvent) => {
+const reviewTextRef = ref<HTMLElement | null>(null);
+
+const seeMoreToggle = async (event : MouseEvent) => {
   event.stopPropagation();
-  showMore.value = !showMore.value;
+  
+  if (!reviewTextRef.value) {
+    showMore.value = !showMore.value;
+    return;
+  }
+  
+  const el = reviewTextRef.value;
+  const startHeight = el.getBoundingClientRect().height;
+  
+  const targetShowMore = !showMore.value;
+  showMore.value = targetShowMore;
+  
+  await nextTick();
+  
+  const endHeight = el.getBoundingClientRect().height;
+  
+  if (!targetShowMore) {
+    showMore.value = true;
+    await nextTick();
+  }
+  
+  const animation = el.animate([
+    { height: `${startHeight}px` },
+    { height: `${endHeight}px` }
+  ], {
+    duration: 350,
+    easing: 'ease-in-out'
+  });
+  
+  animation.onfinish = async () => {
+    if (!targetShowMore) {
+      showMore.value = false;
+    }
+    el.style.height = '';
+  };
 }
 
 </script>
 
 <template>
-  <NuxtLink class="upwork-review-card" target="_blank"
+  <NuxtLink class="upwork-review-card block" target="_blank"
             :href="$config.public.upwork_profile_link">
-    <h4 class="review">
-      <span v-html="comment"></span>
-      <span v-if="contentExceeded" @click.stop.prevent="seeMoreToggle" class="text-[var(--primary-color)] text-lg text-nowrap">&nbsp See {{ showMore ? "less" : "more" }}</span>
-    </h4>
+    <div ref="reviewTextRef" class="overflow-hidden will-change-[height]">
+      <h4 class="review">
+        <span v-html="comment"></span>
+        <span v-if="contentExceeded" @click.stop.prevent="seeMoreToggle" class="text-[var(--primary-color)] text-lg text-nowrap cursor-pointer">&nbsp See {{ showMore ? "less" : "more" }}</span>
+      </h4>
+    </div>
 
     <div class="mt-3" v-if="hasMore">
       <p class="text-[var(--primary-color)] text-lg">Show {{moreCount}} more</p>
